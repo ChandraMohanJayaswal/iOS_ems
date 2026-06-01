90ae6d86c1aba75b9414ed876d56ca7e8022d7fe
bibekchand <bibekchand843@gmail.com>
2026-06-01 16:08:13 +0545

    update

--- ems_ios/View-ViewModel/PersonalLeave/ViewModelPersonalLeave.swift
+++ ems_ios/View-ViewModel/PersonalLeave/ViewModelPersonalLeave.swift
@@ -5,11 +5,15 @@
 //  Created by MacMini on 05/01/2026.
 //
 
-import Foundation
 import Combine
-protocol ViewModelPersonalLeaveServiceProtocol: APIPostPersonalLeave, APIGetLeaveType{}
-final class ViewModelPersonalLeaveService: ViewModelPersonalLeaveServiceProtocol{}
-final class ViewModelPersonalLeave: ObservableObject{
+import Foundation
+
+protocol ViewModelPersonalLeaveServiceProtocol: APIPostPersonalLeave,
+    APIGetLeaveType
+{}
+final class ViewModelPersonalLeaveService: ViewModelPersonalLeaveServiceProtocol
+{}
+final class ViewModelPersonalLeave: ObservableObject {
     @Published var selectedLineManager: Int
     @Published var uiState: UISTATE = .idle
     @Published var selectedLeaveType: Int
@@ -18,7 +22,10 @@ final class ViewModelPersonalLeave: ObservableObject{
     @Published var leaveToDate: Date
     @Published var description: String
     private let apiService: ViewModelPersonalLeaveServiceProtocol
-    init(apiService: ViewModelPersonalLeaveServiceProtocol = ViewModelPersonalLeaveService()){
+    init(
+        apiService: ViewModelPersonalLeaveServiceProtocol =
+            ViewModelPersonalLeaveService()
+    ) {
         self.apiService = apiService
         self.selectedLineManager = 0
         self.selectedLeaveType = 0
@@ -27,25 +34,30 @@ final class ViewModelPersonalLeave: ObservableObject{
         self.description = ""
         self.leaveTypeList = []
     }
-    func fetchLeaveTypeFromServer()async{
+    func fetchLeaveTypeFromServer() async {
         self.uiState = .loading
         self.leaveTypeList.removeAll()
         await apiService.getLeaveType { result in
-            for item in result{
+            for item in result {
                 self.leaveTypeList.append(item)
             }
         }
         self.uiState = .idle
     }
-    func postPersonalLeaveToServer() async{
+    func postPersonalLeaveToServer() async {
         self.uiState = .loading
-        await apiService.postPersonalLeave(selectedLeaveType: self.selectedLeaveType, leaveFromDate: formatDateForServer(self.leaveFromDate), leaveToDate: formatDateForServer(self.leaveToDate), description: self.description)
+        await apiService.postPersonalLeave(
+            selectedLeaveType: self.selectedLeaveType,
+            leaveFromDate: formatDateForServer(self.leaveFromDate),
+            leaveToDate: formatDateForServer(self.leaveToDate),
+            description: self.description
+        )
         self.uiState = .idle
     }
-    func formatDateForServer(_ date: Date) -> String{
+    func formatDateForServer(_ date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        var dateString  = formatter.string(from: date)
+        var dateString = formatter.string(from: date)
         dateString = dateString.replacingOccurrences(of: "/", with: "-")
         return dateString
     }
